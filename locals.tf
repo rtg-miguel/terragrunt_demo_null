bdcc4ac9d138bd3fe44e6b4026ac9b56386dab8b
Miguel <jontiveros@roomstogo.com>
2023-10-02 22:41:40 -0400

    added more outputs

--- locals.tf
+++ locals.tf
@@ -2,6 +2,8 @@ locals{
   decoded_configuration = yamldecode(file(var.configuration_file))
   configuration = {for index, entry in local.decoded_configuration:
     index => {
+      name: index
+      description: "this description can be referenced from the outputs"
     }
   }
 }
